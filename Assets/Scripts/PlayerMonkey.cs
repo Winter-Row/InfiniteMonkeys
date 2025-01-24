7e0f44d3a243a3edaa075b241245405b13964ffc
Tyler Wyer <97559004+tylerwyer@users.noreply.github.com>
2025-01-24 14:02:41 -0500

    Added PlayerCharacter and started scripts

---
+++ Assets/Scripts/PlayerMonkey.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMonkey : MonoBehaviour
+{
+    new Rigidbody2D rigidbody;
+    Animator animator;
+
+    private float moveHorizontal;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Flip character sprite based on movement direction
+        if (moveHorizontal > 0) // Moving right
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (moveHorizontal < 0) // Moving left
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(moveHorizontal * speed, rigidbody.velocity.y);
+    }
+}
