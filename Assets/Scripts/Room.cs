58073f3598c4562fd0dd4d90a79922b672089418
LiamSlappendel <97615261+LiamSlappendel@users.noreply.github.com>
2025-02-23 16:56:48 -0500

    generation
    Added level generation and some rooms

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int topSocket;
+    public int bottomSocket;
+    public int rightSocket;
+    public int leftSocket;
+    public float height;
+    public float width;
+
+    public int inDoor;
+    public int outDoor;
+}
---
+++ Assets/Scripts/SpawnRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour
+{
+    public int topSocket;
+    public int bottomSocket;
+    public int rightSocket;
+    public int leftSocket;
+    public float height;
+    public float width;
+ 
+}
