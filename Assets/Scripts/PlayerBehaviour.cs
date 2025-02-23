cdafc663be18191da2627174adf5a8807ca13250
Winter-Row <winter.e.r@outlook.com>
2025-02-23 18:50:49 -0500

    added another room
    created a 4th room. Also added the walls left and right, plus roof and ground to the prefab folder so building rooms is a little easier

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     public GameObject deadCharacter;
 
     private CheckPoint checkPoint;
+    private Spawn spawn;
     private Vector2 checkPointPos;
     private int health;
     // Start is called before the first frame update
     void Start()
     {
         checkPoint = GameObject.FindGameObjectWithTag("CheckPoint").GetComponent<CheckPoint>();
+        spawn = GameObject.FindGameObjectWithTag("Spawn").GetComponent<Spawn>();
         health = 100;
     }
 
@@ -41,6 +44,13 @@ public class PlayerBehaviour : MonoBehaviour
         if (checkPoint.checkCheckPointed())
         {
             gameObject.transform.position = new Vector2(checkPointPos.x, checkPointPos.y);
+        }
+        else
+        {
+            //for when lives are implemented
+            //gameObject.transform.position = new Vector2(spawn.transform.position.x, spawn.transform.position.y);
+            //for now when player dies take back to the menu
+            SceneManager.LoadScene(0);
         }
     }
 
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     public GameObject playerCharacter;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
+        rend = GetComponent<Renderer>();
         Instantiate(playerCharacter, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
-        gameObject.SetActive(false);
+        rend.enabled = false;
     }
 
     // Update is called once per frame
