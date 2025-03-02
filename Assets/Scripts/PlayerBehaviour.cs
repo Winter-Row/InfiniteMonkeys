11c7ba7b601674d6f4ceeeb09343171698426d2a
Ethan Taylor <estaylor@lakeheadu.ca>
2025-03-02 14:47:15 -0500

    Update PlayerBehaviour.cs

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -24,8 +24,11 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+            onDeath();
+		}
+	}
 
     public void loseHealth(int dmg)
     {
